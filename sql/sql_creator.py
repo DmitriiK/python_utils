230bf8296a93ee7ffb44ba7b175c3269ff0ee40a
dmitrii.kalmanovich <dmitrii_kalmanovich@AVVDID733>
2024-08-23 10:59:55 +0000

    reorganized older files

--- sql/sql_creator.py
+++ sql/sql_creator.py
@@ -3,7 +3,7 @@ from sql.sql_requests import generate_merge_stm
 
 def create_merge_sp(entity_name):
     merge_stm = generate_merge_stm(entity_name +'_tbl')
-    sp_name = '[dbo].[MergeData_{entity_name}_prc]'
+    sp_name = f'[dbo].[MergeData_{entity_name}_prc]'
     create_sp_stm = MERGE_SP.format(sp_name=sp_name, merge_stm = merge_stm)
     with open(spname + '.sql', rw) as f:
         f.write(create_merge_sp)
