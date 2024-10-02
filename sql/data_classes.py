0bc6cadf91a5fc1c2a333f7b2dea2733c0d76601
dmitrii.kalmanovich <dmitrii_kalmanovich@AVVDID733>
2024-10-02 14:32:07 +0000

    SQL_Object data class

---
+++ sql/data_classes.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class DB_Object_Type(Enum):
+    USER_TABLE = 'USER_TABLE'
+    VIEW = 'VIEW'
+    SQL_STORED_PROCEDURE = 'SQL_STORED_PROCEDURE'
+    SQL_SCALAR_FUNCTION = 'SQL_SCALAR_FUNCTION'  
+    SQL_INLINE_TABLE_VALUED_FUNCTION = 'SQL_INLINE_TABLE_VALUED_FUNCTION'
+    CLR_STORED_PROCEDURE = 'CLR_STORED_PROCEDURE'
+
+
+@dataclass
+class SQL_Object:
+    object_id: int
+    name: str
+    schema: str = 'dbo'
+    db_name: str = None
+    server_name: str = None
+    type: DB_Object_Type
