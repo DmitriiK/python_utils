6d866504eae22ec310a1c734ab95f9cc897c6638
dmitrii.kalmanovich <dmitrii.kalmanovich@spglobal.com>
2025-02-18 12:01:13 +0000

    batch table creation

--- tests/test_xfc_package_creation.py
+++ tests/test_xfc_package_creation.py
@@ -4,7 +4,7 @@ from sql.sql_requests import SQL_Communicator
 from  sql.xfc_package_creation import create_xfc_ds_table, XFC_Creator, XFC_SourceDB, TransDeliveryStyle
 class TestXFCCreation(unittest.TestCase):
     def test_create_source_table(self):
-        table_name = 'IDCEquitySecurity_TradingItem2_tbl'
+        table_name = 'IDCEquitySecurityToSecurityFeature2_tbl'
         sql = create_xfc_ds_table(source_table_name=table_name, 
                                      xfc_database_id=XFC_SourceDB.DataFeedEngineMI, 
                                      tds=TransDeliveryStyle.FULL, 
@@ -18,3 +18,29 @@ class TestXFCCreation(unittest.TestCase):
             xfc_cr = XFC_Creator(sqlc)
             sql = xfc_cr.create_xfc_ds_table_columns(xfc_database_id=XFC_SourceDB.DataFeedEngineMI, source_table_name=table_name)       
             pyperclip.copy(sql)
+
+ 
+
+
+
+    def test_create_source_table_and_columns(self):
+        tbls  = [
+            ('IDCEquitySecurityToSecurityFeature2_tbl', 'EqSecurity', TransDeliveryStyle.CHANGE),
+            ('IDCEquitySecurity_Security2_tbl', 'EqSecurity', TransDeliveryStyle.CHANGE),
+            ('RefData_SecurityGroup_tbl', 'EqSecurity', TransDeliveryStyle.FULL),
+            ('RefData_SecuritySubType_tbl', 'EqSecurity', TransDeliveryStyle.FULL),
+        ]
+        db_id = XFC_SourceDB.DataFeedEngineMI
+        sql_script = ''
+        with SQL_Communicator() as sqlc:
+            xfc_cr = XFC_Creator(sqlc)
+            for table_name, pop_gr, tds in tbls:
+                sql = create_xfc_ds_table(source_table_name=table_name, 
+                                        xfc_database_id=db_id, 
+                                        tds=tds, 
+                                        population_group=pop_gr) 
+                sql_script += '\nGO\n' +sql
+
+                sql = xfc_cr.create_xfc_ds_table_columns(xfc_database_id=XFC_SourceDB.DataFeedEngineMI, source_table_name=table_name) 
+                sql_script += '\nGO\n' +sql      
+        pyperclip.copy(sql_script) 
