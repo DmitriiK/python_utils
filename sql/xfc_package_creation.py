c88131247020dea80d1cb388f0d511d10241f165
dmitrii.kalmanovich <dmitrii.kalmanovich@spglobal.com>
2025-02-18 09:57:59 +0000

    refactoring

--- sql/xfc_package_creation.py
+++ sql/xfc_package_creation.py
@@ -10,6 +10,65 @@ class TransDeliveryStyle(Enum):
     CHANGE = 'CHANGE'
     FULL = 'FULL'
 
+class XFC_Creator:
+    def __init__(self, sql_dt: SQL_Communicator):
+        self.sql_dt=sql_dt
+
+ 
+    def create_xfc_ds_table_columns(self, source_table_name: str, xfc_database_id: XFC_SourceDB):
+        def xfc_data_type(col) -> str:
+            return f"'{col.data_type}{f'({col.precision})' if 'char' in col.data_type else ''}'"
+        def xfc_data_key_flag(col) -> str:
+            return "'y'" if col.is_in_pk else "''"
+        
+        tbl, sch = get_table_schema(source_table_name)
+        cols = self.sql_dt.get_columns(table_name=source_table_name)
+        col_lst = [f"('{col.column_name}', {xfc_data_type(col)}, {xfc_data_key_flag(col)})" 
+                    for col in cols]
+        cols_vals = '\n\t\t,'.join(col_lst)
+        sql = f"""
+    DECLARE @source_table_id int 
+    SELECT top 1  @source_table_id = sourceTableId FROM xfc_SourceTableProfile t 
+            WHERE t.sourceTableName ='{tbl}' AND t.sourceTableSchema = '{sch}' AND DataBaseId= {xfc_database_id.value}   
+            ORDER BY sourceTableId DESC;
+    IF @source_table_id IS NULL  BEGIN
+        DECLARE @msg nvarchar(100)
+        SELECT @msg = FORMATMESSAGE('Table for %s has not been found in xfc_SourceTableProfile', '{source_table_name}' );
+        THROW 50005,  @msg , 1;
+    END
+
+    DECLARE @max_id int
+    SELECT @max_id = MAX(scp.sourceColumnId) FROM xfc_SourceColumnProfile scp;
+
+    WITH cte_fields AS (
+        SELECT *
+        FROM (VALUES
+        {cols_vals}
+        ) AS cte_fields(field_name, data_type, is_in_pk)
+    )
+    ,new_fls as
+    (SELECT * from cte_fields f 
+        WHERE f.field_name 	NOT IN (SELECT sourceColumnName  FROM xfc_SourceColumnProfile scp where scp.sourceTableId = @source_table_id)
+        )
+
+    ,cte_f_rn AS (
+    SELECT 
+        ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS rn,
+        * FROM new_fls
+        )
+    --final write
+    INSERT INTO xfc_SourceColumnProfile(sourceColumnId, sourceTableId, sourceColumnName, DataType, KeyFlag, indexColumnOrder)
+    SELECT 
+        rn + @max_id as new_id,
+        @source_table_id as source_table_id, 
+        field_name, data_type, 
+        is_in_pk,
+        CASE WHEN is_in_pk = 'y' THEN rn ELSE '' END as index_col_order
+    FROM cte_f_rn
+    """
+        return sql
+
+       
 def create_xfc_ds_table(source_table_name: str, xfc_database_id: XFC_SourceDB, tds: TransDeliveryStyle, population_group: str):
 
     tbl, sch = get_table_schema(source_table_name)
@@ -29,63 +88,4 @@ def create_xfc_ds_table(source_table_name: str, xfc_database_id: XFC_SourceDB, t
             UPDATE stp  SET stp.TransDeliveryStyle =  'FULL', stp.PopGroup='EqSecurity'FROM  xfc_SourceTableProfile stp WHERE sourceTableId=@source_table_id
 
         """
-
     return sql
-
-
-def create_xfc_ds_table_columns(source_table_name: str, source_table_id: int, ):
-    def xfc_data_type(col) -> str:
-        return f"'{col.data_type}{f'({col.precision})' if 'char' in col.data_type else ''}'"
-    def xfc_data_key_flag(col) -> str:
-        return "'y'" if col.is_in_pk else "''"
-    
-    with SQL_Communicator() as sql_dt:
-        cols = sql_dt.get_columns(table_name=source_table_name)
-        print(cols)
-    col_lst = [f"('{col.column_name}', {xfc_data_type(col)}, {xfc_data_key_flag(col)})" 
-                for col in cols]
-
-    cols_vals = '\n, '.join(col_lst)
-
-    sql = f"""
-DECLARE @source_table_id int ;
-SELECT @source_table_id  = {source_table_id} 
-
-DECLARE @max_id int
-SELECT @max_id = MAX(scp.sourceColumnId) FROM xfc_SourceColumnProfile scp;
-
-WITH cte_fields AS (
-    SELECT *
-    FROM (VALUES
-       {cols_vals}
-    ) AS cte_fields(field_name, data_type, is_in_pk)
-)
-,new_fls as
-(SELECT * from cte_fields f 
-	WHERE f.field_name 	NOT IN (SELECT sourceColumnName  FROM xfc_SourceColumnProfile scp where scp.sourceTableId = @source_table_id)
-	)
-
-,cte_f_rn AS (
-SELECT 
-     ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS rn,
-    * FROM new_fls
-	)
---final write
-INSERT INTO xfc_SourceColumnProfile(sourceColumnId, sourceTableId, sourceColumnName, DataType, KeyFlag, indexColumnOrder)
-SELECT 
-	rn + @max_id as new_id,
-	@source_table_id as source_table_id, 
-	field_name, data_type, 
-	is_in_pk,
-	CASE WHEN is_in_pk = 'y' THEN rn ELSE '' END as index_col_order
-FROM cte_f_rn
-"""
-
-    return sql
-
-
-
-
-"""exec sp_executesql N'insert into xfc_SourceColumnProfile 
-(columnFormatId, DataType, indexColumnOrder, KeyFlag, popQueryFlag, sourceColumnName, sourceColumnId, sourceTableId) values 
-,N'@P0 int,@P1 nvarchar(4000),@P2 smallint,@P3 nvarchar(4000),@P4 bit,@P5 nvarchar(4000),@P6 int,@P7 int',NULL,N'int',1,N'Y',0,N'companyID',13786,7934"""
--- tests/test_xfc_package_creation.py
+++ tests/test_xfc_package_creation.py
@@ -1,6 +1,7 @@
 import unittest
 import pyperclip
-from  sql.xfc_package_creation import create_xfc_ds_table, create_xfc_ds_table_columns, XFC_SourceDB, TransDeliveryStyle
+from sql.sql_requests import SQL_Communicator
+from  sql.xfc_package_creation import create_xfc_ds_table, XFC_Creator, XFC_SourceDB, TransDeliveryStyle
 class TestXFCCreation(unittest.TestCase):
     def test_create_source_table(self):
         table_name = 'IDCEquitySecurity_TradingItem2_tbl'
@@ -12,8 +13,8 @@ class TestXFCCreation(unittest.TestCase):
         pyperclip.copy(sql)
 
     def test_create_source_table_columns(self):
-        table_name, tbl_id = 'IDCEquitySecurity_TradingItem2_tbl', 8048
-        sql = create_xfc_ds_table_columns(source_table_name=table_name, 
-                                          source_table_id=tbl_id)
-        
-        pyperclip.copy(sql)
+        table_name ='IDCEquitySecurity_TradingItem2_tbl'
+        with SQL_Communicator() as sqlc:
+            xfc_cr = XFC_Creator(sqlc)
+            sql = xfc_cr.create_xfc_ds_table_columns(xfc_database_id=XFC_SourceDB.DataFeedEngineMI, source_table_name=table_name)       
+            pyperclip.copy(sql)
